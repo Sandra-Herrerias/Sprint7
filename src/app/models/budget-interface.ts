943784073483dd2e74dc1ae7ddd911a76655c6f1
SandraHerrerias <s.herreriasrodriguez@gmail.com>
2022-11-29 22:57:52 +0100

    modal backdrop changed

---
+++ src/app/models/budget-interface.ts
@@ -0,0 +1,8 @@
+export interface BudgetInterface {
+    id:number;
+    budget_name: string;
+    user_name: string;
+    services_selected: string[];
+    total_price: number;
+    date: string;
+}
