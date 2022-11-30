243fe8633cd27196fabb812eea0632f9c2295ac8
SandraHerrerias <s.herreriasrodriguez@gmail.com>
2022-11-30 09:15:20 +0100

    nivell 2 finalitzat

--- src/app/models/budget-interface.ts
+++
@@ -1,8 +0,0 @@
-export interface BudgetInterface {
-    id:number;
-    budget_name: string;
-    user_name: string;
-    services_selected: string[];
-    total_price: number;
-    date: string;
-}
